f57aa4f943e0edbacbe7e2214dbc10b787d11ba7
belindanju <tian@cs.umass.edu>
2014-03-27 18:08:19 -0400

    Update xmlclient.py
    clean up xmlclient

--- xmlclient.py
+++ xmlclient.py
@@ -5,15 +5,15 @@ __date__ = '3/10/14'
 
 import xmlrpclib
 
-s = xmlrpclib.ServerProxy('http://localhost:18000')
+# make the server_ip and server_port known to client 
+server_ip = 'localhost'
+server_port = 18000
 
-print s.system.listMethods()
+rpc_server = xmlrpclib.ServerProxy("http://%s:%d"%(server_ip, server_port))  # address is a tuple
 
-print s.adder_function(2,3)
+print rpc_server.system.listMethods()  # list all the available funcs
 
-#print s.add(2,3)
-
-print s.lookup()
-
-print s.sub(3,2)
-print s.hello('your name')
+print rpc_server.add(2,3)
+print rpc_server.lookup()
+print rpc_server.sub(3,2)
+print rpc_server.hello('your name')
