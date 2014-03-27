6cbd8e73113d7660f7402eacf15085733ec0e442
TG <tian@cs.umass.edu>
2014-03-27 18:10:00 -0400

    name the xmlserver

--- xmlserver.py
+++ xmlserver.py
@@ -19,7 +19,7 @@ class MyXMLServer():
         self.name = name 
            
     def lookup(self):  # an instance function takes no args, but return a string
-        return "my name %s" % self.name
+        return "my name is %s" % self.name
 
     def add(self, x, y):  # an instance function takes args, returns integer
         return x + y
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     server.register_introspection_functions()  # important! 
                       
     # register instances, all the functions inside is available
-    server.register_instance(MyXMLServer())
+    server.register_instance(MyXMLServer('simple server'))
     
     # register functions
     server.register_function(substract, 'sub')  # expose substract as function sub
